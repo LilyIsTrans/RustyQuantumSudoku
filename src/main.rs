fbad47c72e1ad2bb4e8c8af630e18589690657cf
Rilazy <rileyjohn31@gmail.com>
2023-04-06 19:26:39 -0400

    Leaving

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
 use ::num::complex::Complex;
 
-const MAX_ITERATIONS: u64 = 200;
+enum EscapeTime {
+    Iterations(usize),
+    Never
+}
 
 fn mandelbrot_iteration(z: Complex<f64>, c: Complex<f64>) -> Complex<f64> {
     z * z + c
 }
 
-fn mandelbrot_iteration_count(c: Complex<f64>) -> u64 {
-    let mut iterations: u64 = 0;
+fn mandelbrot_iteration_count(c: Complex<f64>) -> EscapeTime {
+    const MAX_ITERATIONS: usize = 200;
+    let mut iterations: usize = 0;
     let mut z: Complex<f64> = Complex { re: (0f64), im: (0f64) };
     while z.norm_sqr() < 4.0 && iterations < MAX_ITERATIONS {
         z = mandelbrot_iteration(z, c);
         iterations += 1;
     }
-    iterations
+    match iterations {
+        
+    }
 }
+
+
 
 fn main() {
     println!("Hello, world!");
