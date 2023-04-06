9947a4a0d6c04009dffcc36d1ebb0b6380e282fe
Rilazy <rileyjohn31@gmail.com>
2023-04-06 19:20:18 -0400

    Add some basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use ::num::complex::Complex;
+
+const MAX_ITERATIONS: u64 = 200;
+
+fn mandelbrot_iteration(z: Complex<f64>, c: Complex<f64>) -> Complex<f64> {
+    z * z + c
+}
+
+fn mandelbrot_iteration_count(c: Complex<f64>) -> u64 {
+    let mut iterations: u64 = 0;
+    let mut z: Complex<f64> = Complex { re: (0f64), im: (0f64) };
+    while z.norm_sqr() < 4.0 && iterations < MAX_ITERATIONS {
+        z = mandelbrot_iteration(z, c);
+        iterations += 1;
+    }
+    iterations
+}
+
 fn main() {
     println!("Hello, world!");
 }
